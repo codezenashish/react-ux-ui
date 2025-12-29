78b7dc01c0f4ef945a76985c3938bbdb2739b999
codezenashish <ixtashish@gmail.com>
2025-12-29 23:50:56 +0530

    Enhance Hero component with mouse position tracking and dynamic background effect

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,36 @@
+import { useEffect, useState } from "react";
+
 const Hero = () => {
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    function handleMouseMove(event) {
+      setMousePosition({ x: event.clientX, y: event.clientY });
+    }
+    window.addEventListener("mousemove", handleMouseMove);
+    return () => window.removeEventListener("mousemove", handleMouseMove);
+  }, []);
   return (
-    <div>Hero</div>
-  )
-}
-export default Hero
+    <section className="relative min-h-screen flex items-center justify-center pt-16 sm:pt-20 px-4 sm:px-6 lg:px-8 overflow-hidden">
+      <div
+        className="absolute inset-0 opacity-30"
+        style={{
+          background: `radial-gradient(600px circle at ${mousePosition.x}px ${mousePosition.y}px, rgba(59,130,246,0.15), transparent 40%`,
+        }}
+      ></div>
+
+      <div className="absolute top-20 left-4 sm:left-10 w-48 sm:w-72 h-48 sm:h-72 bg-blue-500/10 rounded-full blur-3xl animate-pulse"></div>
+      <div className="absolute bottom-20 right-4 sm:right-10 w-48 sm:w-72 h-48 sm:h-72 bg-cyan-500/10 rounded-full blur-3xl animate-pulse delay-1000"></div>
+
+      <div className=" relative order-2 w-full ">
+        <div className="relative bg-white/5 backdrop-blur-xl rounded-xl sm:rounded-2xl p-3 sm:p-4 border border-white/10">
+          <div className="bg-linear-to-br from-gray-900/20 to-gray-800/20 backdrop-blur-sm rounded-lg overflow-hidden h-[250px] w-[350px] lg:h-[450px]">
+            {/* ide header */}
+            <div className="flex items-center justify-between px-3 sm:px-4 py-2 sm:3 bg-white/5 backdrop-blur-sm border-b border-white/10"></div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default Hero;
